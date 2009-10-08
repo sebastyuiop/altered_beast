442c76cebc3ecc070548d1619e4a57b2934ddfae
Seb Weston <sebweston@seb-westons-macbook-pro.local>
2009-10-08 15:25:54 +0100

    Removed dupicate validation for topic title as it also appears on line 32

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,6 @@ class Topic < ActiveRecord::Base
   include User::Editable
 
   before_validation_on_create :set_default_attributes
-  validates_presence_of :title
 
   after_create   :create_initial_post
   before_update  :check_for_moved_forum
